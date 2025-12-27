3263d1a633f261a09ecb95ad1a533c954a285c04
irfanrumman <mdirfansayid@gmail.com>
2025-12-27 23:40:11 +0600

    bug fixed

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,55 +1,69 @@
-// fetch korbo
+// fetching variables
 const mealslist = document.querySelector(".meals-area");
+const spinner = document.getElementById("spinner");
 const recipesModal = document.querySelector(".recipesmodal-area");
 
-async function getMeals() {
-  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+// search box variables
+const inputBox = document.querySelector(".form-control");
+const searchBtn = document.querySelector(".searchbtn");
+
+// fetching funtion
+async function getMeals(inputValue = "") {
+  spinner.style.display = "block";
+  mealslist.innerHTML = "";
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
   const response = await fetch(url);
   const data = await response.json();
-  data.meals
-    .map((foods) => {
-      const recipeDiv = document.createElement("div");
-      recipeDiv.classList.add("recipes");
-      recipeDiv.innerHTML = `<img src="${foods.strMealThumb}" alt="">
-      <h2>${foods.strMeal}</h2> 
+
+  spinner.style.display = "none";
+
+  if (!data.meals) {
+    mealslist.innerHTML = `<p class="nodataFound">No meals found! Try another search...</p>`;
+    return;
+  }
+
+  data.meals.forEach((foods) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipes");
+    recipeDiv.innerHTML = `<img src="${foods.strMealThumb}" alt="">
+      <h2>${foods.strMeal}</h2>
       <p>${foods.strInstructions}</p>
       <button>View Details</button>`;
-      const button = recipeDiv.querySelector("button");
-      button.addEventListener("click", (e) => {
-        const modalPoupRecipes = document.createElement("div");
-        modalPoupRecipes.classList.add("recipesDetails");
-        modalPoupRecipes.innerHTML = `
+    const button = recipeDiv.querySelector("button");
+    button.addEventListener("click", (e) => {
+      const modalPoupRecipes = document.createElement("div");
+      modalPoupRecipes.classList.add("recipesDetails");
+      modalPoupRecipes.innerHTML = `
         <img src="${foods.strMealThumb}" alt="">
-        <h2>${foods.strMeal}</h2> 
+        <h2>${foods.strMeal}</h2>
         <p>${foods.strInstructions}</p>
         <button class="closeBtn">Close</button>
       `;
-        const closeBtn = modalPoupRecipes.querySelector(".closeBtn");
-        closeBtn.addEventListener("click", () => {
-          recipesModal.style.display = "none";
-        });
-        recipesModal.innerHTML = "";
-        recipesModal.style.display = "block";
-        recipesModal.append(modalPoupRecipes);
+      const closeBtn = modalPoupRecipes.querySelector(".closeBtn");
+      closeBtn.addEventListener("click", () => {
+        recipesModal.style.display = "none";
       });
-      mealslist.append(recipeDiv);
-    })
-    .join("");
+      recipesModal.innerHTML = "";
+      recipesModal.style.display = "block";
+      recipesModal.append(modalPoupRecipes);
+    });
+    mealslist.appendChild(recipeDiv);
+  });
 }
 getMeals();
 
-async function foodname() {
-  const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`"
-  );
-  const data = await response.json();
-}
-foodname();
+// search event
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  const inputValue = inputBox.value.trim();
+  getMeals(inputValue);
+});
 
-async function invalidId() {
-  const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`"
-  );
-  const data = await response.json();
-}
-invalidId();
+// Enter key press event
+inputBox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault;
+    const inputValue = inputBox.value.trim();
+    getMeals(inputValue);
+  }
+});
