69867aa3c075c7f493dffe2e2f11eee236400a21
irfanrumman <mdirfansayid@gmail.com>
2025-12-27 07:56:00 +0600

    Fixed bugs

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,12 +1,39 @@
 // fetch korbo
+const mealslist = document.querySelector(".meals-area");
+const recipesModal = document.querySelector(".recipesmodal-area");
 
 async function getMeals() {
   const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-  document.querySelector("#foods").innerHTML = data.meals
-    .map((foods) => `<div>${foods.idMeal} ${foods.strMeal}</div>`)
+  data.meals
+    .map((foods) => {
+      const recipeDiv = document.createElement("div");
+      recipeDiv.classList.add("recipes");
+      recipeDiv.innerHTML = `<img src="${foods.strMealThumb}" alt="">
+      <h2>${foods.strMeal}</h2> 
+      <p>${foods.strInstructions}</p>
+      <button>View Details</button>`;
+      const button = recipeDiv.querySelector("button");
+      button.addEventListener("click", (e) => {
+        const modalPoupRecipes = document.createElement("div");
+        modalPoupRecipes.classList.add("recipesDetails");
+        modalPoupRecipes.innerHTML = `
+        <img src="${foods.strMealThumb}" alt="">
+        <h2>${foods.strMeal}</h2> 
+        <p>${foods.strInstructions}</p>
+        <button class="closeBtn">Close</button>
+      `;
+        const closeBtn = modalPoupRecipes.querySelector(".closeBtn");
+        closeBtn.addEventListener("click", () => {
+          recipesModal.style.display = "none";
+        });
+        recipesModal.innerHTML = "";
+        recipesModal.style.display = "block";
+        recipesModal.append(modalPoupRecipes);
+      });
+      mealslist.append(recipeDiv);
+    })
     .join("");
 }
 getMeals();
